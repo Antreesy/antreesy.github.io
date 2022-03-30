a99ac3254bcb27d1496f35538cb9cc8eaca96185
Antreesy <93392545+Antreesy@users.noreply.github.com>
2022-03-30 11:24:36 +0700

    Delete projects/react-app directory

--- projects/react-app/src/Interfaces/IAuthContextType.ts
+++
@@ -1,5 +0,0 @@
-export default interface AuthContextType {
-    user: string | null;
-    signin: (user: string, callback: VoidFunction) => void;
-    signout: (callback: VoidFunction) => void;
-  }
--- projects/react-app/src/Methods/UseAuth.ts
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from "react";
-import AuthContextType from "../Interfaces/IAuthContextType";
-
-export const AuthContext = createContext<AuthContextType>(null!);
-
-export function useAuth() {
-  return useContext(AuthContext);
-}
